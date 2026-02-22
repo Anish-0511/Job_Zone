449505b85ff71f4ca783f05b61bcb92929ebb9a6
Anish Kumar Singh <singhrajputanish535@gmail.com>
2026-02-22 21:24:59 +0530

    Update footer copyright and social links

--- Frontend/src/components/Layout/Footer.jsx
+++ Frontend/src/components/Layout/Footer.jsx
@@ -8,18 +8,18 @@ const Footer = () => {
   const { isAuthorized } = useContext(Context);
   return (
     <footer className={isAuthorized ? "footerShow" : "footerHide"}>
-      <div>&copy; All Rights Reserved By SimranRajput.</div>
+      <div>&copy; All Rights Reserved By Anish Kumar Singh.</div>
       <div>
-        <Link to={"https://www.facebook.com/simransingh.rajput.9277?mibextid=ZbWKwL"} target="_blank">
+        <Link to={"https://www.facebook.com/anish.raj.7330"} target="_blank">
           <FaFacebookF />
         </Link>
-        <Link to={"https://www.youtube.com/@simransingh1099"} target="_blank">
+        <Link to={"https://www.youtube.com/"} target="_blank">
           <FaYoutube />
         </Link>
-        <Link to={"https://www.linkedin.com/in/simran-29as09"} target="_blank">
+        <Link to={"https://www.linkedin.com/in/anish-kumar-singh-5baa68275/"} target="_blank">
           <FaLinkedin />
         </Link>
-        <Link to={"https://www.instagram.com/simran._rajput_"} target="_blank">
+        <Link to={"https://www.instagram.com/"} target="_blank">
           <RiInstagramFill />
         </Link>
       </div>
